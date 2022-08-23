def4737cae6ce262aea1a2e5e666bcfb9d9e6135
zgvr81 <hddr09@gmail.com>
2022-08-23 19:38:14 +0200

    init

--- video-store-offline/sw.js
+++ video-store-offline/sw.js
@@ -2,10 +2,10 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       '/learning-area/javascript/apis/client-side-storage/cache-sw/video-store-offline/',
-       '/learning-area/javascript/apis/client-side-storage/cache-sw/video-store-offline/index.html',
-       '/learning-area/javascript/apis/client-side-storage/cache-sw/video-store-offline/index.js',
-       '/learning-area/javascript/apis/client-side-storage/cache-sw/video-store-offline/style.css'
+       '/ifksp/zgvr81.github.io/video-store-offline/',
+       '/ifksp/zgvr81.github.io/video-store-offline/index.html',
+       '/ifksp/zgvr81.github.io/video-store-offline/index.js',
+       '/ifksp/zgvr81.github.io/video-store-offline/style.css'
      ]);
    })
  );
